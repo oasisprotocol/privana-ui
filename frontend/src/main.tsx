d462346f316e490b9ec20aff4112413e5809c292
Michal Zielenkiewicz <michal@oasisprotocol.org>
2026-02-03 09:19:41 +0100

    Customize RainbowKit theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,18 +4,27 @@ import { RouterProvider } from 'react-router'
 import { router } from './routes'
 import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { darkTheme, RainbowKitProvider, Theme } from '@rainbow-me/rainbowkit'
 import { wagmiConfig } from './wagmi-config.ts'
 import '@rainbow-me/rainbowkit/styles.css'
 import './index.css'
 
 const queryClient = new QueryClient()
 
+const rainbowKitTheme: Theme = {
+  ...darkTheme({
+    accentColor: 'rgba(255, 255, 255, 0.60)',
+  }),
+  fonts: {
+    body: 'inherit',
+  },
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
+        <RainbowKitProvider theme={rainbowKitTheme} modalSize="compact">
           <RouterProvider router={router} />
         </RainbowKitProvider>
       </QueryClientProvider>
