60d6557331af535343832692d8ad59d31d8d01f6
buberdds <michal@oasisprotocol.org>
2026-02-12 10:22:49 +0100

    Prevent showing default react-router error boundary

---
+++ frontend/src/components/NotFound/index.tsx
@@ -0,0 +1,14 @@
+import { Layout } from '../../components/Layout'
+
+export function NotFound() {
+  return (
+    <Layout dashboard>
+      <div className="flex flex-col flex-1 gap-4 justify-center items-center">
+        <span className="text-foreground text-2xl font-medium">Page Not Found</span>
+        <span className="text-sm text-muted-foreground">
+          The page you are looking for does not exist. Please check the URL and try again.
+        </span>
+      </div>
+    </Layout>
+  )
+}
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,6 +5,7 @@ import { DashboardHome } from './pages/Dashboard/DashboardHome'
 import { Deposit } from './pages/Dashboard/Deposit'
 import { CopyTrading } from './pages/CopyTrading'
 import { CreateStrategy } from './pages/CreateStrategy'
+import { NotFound } from './components/NotFound'
 
 export const router = createBrowserRouter([
   {
@@ -39,5 +40,9 @@ export const router = createBrowserRouter([
         element: <CreateStrategy />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
