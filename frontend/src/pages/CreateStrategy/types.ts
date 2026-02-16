24c6c01c2439d0b6a727d848519fe02502b83bfb
buberdds <michal@oasisprotocol.org>
2026-02-16 13:40:45 +0100

    Add logic to choose traders step

--- frontend/src/pages/CreateStrategy/types.ts
+++ frontend/src/pages/CreateStrategy/types.ts
@@ -2,5 +2,17 @@ export type StrategyData = {
   name: string
   amount: string
   token: string
-  traders: { address: string; allocation: string }[]
+  traders: { address: string; allocation: number }[]
+}
+
+export type TraderDisplayData = {
+  id: string
+  address: string
+  lastTrade: string
+  size: string
+  monthlyPnl: string
+}
+
+export type TradersFormValues = {
+  traders: { address: string; allocation: number }[]
 }
