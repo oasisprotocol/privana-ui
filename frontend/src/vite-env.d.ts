d2b9de11ce3ac24efcf7530d94e2c5c422f614ea
buberdds <michal@oasisprotocol.org>
2026-04-09 13:54:14 +0200

    Move allowed tokens id to env file

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -12,6 +12,7 @@ interface ImportMetaEnv {
   readonly VITE_ACCOUNTING_CONTRACT_ADDRESS: `0x${string}`
   readonly VITE_FLEXVAULTS_API_URL: string
   readonly VITE_FLEXVAULTS_CLIENT_ID: string
+  readonly VITE_FLEXVAULTS_ALLOWED_TOKENS: string
 }
 
 interface ImportMeta {
