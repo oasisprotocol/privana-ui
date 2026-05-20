36d1107f6e3c735cb3d509b56e053b65f5f2519a
buberdds <michal@oasisprotocol.org>
2026-05-20 08:54:47 +0200

    Build wagmi config for Turnkey migration

--- frontend/src/wagmi-config.ts
+++ frontend/src/wagmi-config.ts
@@ -1,20 +1,27 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { createConfig, http } from 'wagmi'
 import { sapphire, sapphireTestnet, baseSepolia } from 'viem/chains'
+import { injected, walletConnect } from 'wagmi/connectors'
 
 const { VITE_WALLET_CONNECT_PROJECT_ID } = import.meta.env
 
-declare module 'wagmi' {
-  interface Register {
-    config: ReturnType<typeof getDefaultConfig>
-  }
-}
-
-export const wagmiConfig: ReturnType<typeof getDefaultConfig> = getDefaultConfig({
-  appName: 'Flex Vaults',
-  projectId: VITE_WALLET_CONNECT_PROJECT_ID,
+export const wagmiConfig = createConfig({
   chains: [sapphire, sapphireTestnet, baseSepolia],
-  ssr: false,
+  connectors: [injected(), walletConnect({ projectId: VITE_WALLET_CONNECT_PROJECT_ID })],
+  transports: {
+    [sapphire.id]: http(),
+    [sapphireTestnet.id]: http(),
+    [baseSepolia.id]: http(),
+  },
   batch: {
     multicall: false,
   },
+  ssr: false,
 })
+
+export type AppChainId = (typeof wagmiConfig)['chains'][number]['id']
+
+declare module 'wagmi' {
+  interface Register {
+    config: typeof wagmiConfig
+  }
+}
